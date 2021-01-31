d764938b88fe7bd9831733fc848868456a9884f3
Paulo Cândido <paulo.candido@g.globo>
2021-01-30 21:30:56 -0300

    new canvas class

---
+++ src/com/paulocandido/ui/Canvas.java
@@ -0,0 +1,31 @@
+package com.paulocandido.ui;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class Canvas extends JPanel {
+
+    private Image image;
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(
+                this.image,
+                0,
+                0,
+                this.getWidth(),
+                this.getHeight(),
+                0,
+                0,
+                image.getWidth(null),
+                image.getHeight(null),
+                null
+        );
+    }
+}
