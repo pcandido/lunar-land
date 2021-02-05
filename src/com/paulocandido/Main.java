fa872721cf9ab358cba3cdbf4602022b27b98e9a
Paulo Cândido <paulo.candido@g.globo>
2021-02-05 20:27:28 -0300

    refactor: organize the main class

--- src/com/paulocandido/Main.java
+++ src/com/paulocandido/Main.java
@@ -6,28 +6,33 @@ import com.paulocandido.ui.UI;
 
 import java.io.IOException;
 
-public class Main {
+public class Main extends Thread {
 
     public static void main(String[] args) throws IOException {
-
-        var moon = new Moon("map.png");
-        var population = new Population(50, 1);
-        new UI(moon,population );
-
-        new Thread(() -> {
-            while (true) {
-                population.update(moon);
-
-                try {
-                    //noinspection BusyWait
-                    Thread.sleep(20);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    System.exit(1);
-                }
-            }
-        }).start();
-
+        new Main("moon1.png", 50, 1).start();
     }
 
+    private Moon moon;
+    private Population population;
+
+    public Main(String moonFileName, int populationSize, double startingNoise) throws IOException {
+        this.moon = new Moon(moonFileName);
+        this.population = new Population(populationSize, startingNoise);
+        new UI(this.moon, this.population);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            this.population.update(moon);
+
+            try {
+                //noinspection BusyWait
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
 }
