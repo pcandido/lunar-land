cfb1067a0167b5ea032c85bedad6d33fe910b574
Paulo Cândido <paulo.candido@g.globo>
2021-02-05 21:15:01 -0300

    refactor: organize the Main class

--- src/com/paulocandido/Main.java
+++ src/com/paulocandido/Main.java
@@ -9,15 +9,26 @@ import java.io.IOException;
 public class Main extends Thread {
 
     public static void main(String[] args) throws IOException {
-        new Main("moon1.png", 50, 1).start();
+        new Main(
+                new Moon(
+                        "moon1.png",
+                        0.003,
+                        0.001
+                ),
+                new Population(
+                        50,
+                        1,
+                        1500
+                )
+        ).start();
     }
 
     private Moon moon;
     private Population population;
 
-    public Main(String moonFileName, int populationSize, double startingNoise) throws IOException {
-        this.moon = new Moon(moonFileName);
-        this.population = new Population(populationSize, startingNoise);
+    public Main(Moon moon, Population population) throws IOException {
+        this.moon = moon;
+        this.population = population;
         new UI(this.moon, this.population);
     }
 
