14861ffca7020d18c6e9b5049d47f25dcf1e6394
Paulo Cândido <paulo.candido@g.globo>
2021-02-09 23:11:39 -0300

    reduce the max number of mutations

--- src/com/paulocandido/Config.java
+++ src/com/paulocandido/Config.java
@@ -2,7 +2,7 @@ package com.paulocandido;
 
 public class Config {
 
-    public final static int MAX_NUMBER_MUTATIONS = 15;
+    public final static int MAX_NUMBER_MUTATIONS = 10;
     public final static int POPULATION_SIZE = 2000;
 
 }
